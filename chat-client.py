d7b46f4c32528ca8721682ca3df56a3537e5ebe8
JadeHarbert <harbertjade2@gmail.com>
2022-10-10 20:14:31 -0400

    Finished Product

--- chat-client.py
+++ chat-client.py
@@ -1,4 +1,7 @@
 # chat-client.py
+# Jade Harbert
+# CSC122
+# October 10th, 2022
 
 import socket
 
@@ -7,16 +10,41 @@ PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    print("Welcome to the chat room! \nType EXIT to quit")
-    while True:
+
+    # Variable used to determine if the chat session is ending
+    isConnected = True
+
+    print("Welcome to the chat room! \n"
+          "Type EXIT to end session \n"
+          "Put a period at the end of chat to send message")
+
+    # While the chat session is still going
+    while isConnected:
         msg = input("Enter Msg: ")
+
+        # If the client wants to end the session
         if msg == "EXIT":
+            msg = msg.encode()
+            s.sendall(msg)
             break
+
+        # Checks to see if the client wants to send their message.
+        # Continuously asks for input until the client is ready to send
+        while msg[-1] != ".":
+            msg += "\n\t\t\t" + input()
+
+        # Sends message
         msg = msg.encode()
         s.sendall(msg)
+
+        # Receives message from server
         data = s.recv(1024)
         data = data.decode()
 
-        if data == "EXIT":
-            break
-        print(data)
+        # If the server wants to end the chat session, then we close the connection
+        # We also set the connection variable to false so the program ends
+        if data == "EXIT" or data == "QUIT":
+            s.close()
+            isConnected = False
+        else:
+            print("From Server:" + data)
--- chat-server.py
+++ chat-server.py
@@ -1,25 +1,87 @@
 # chat-server.py
+# Jade Harbert
+# CSC122
+# October 10th, 2022
 
 import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"connection from {addr}")
-        print("Welcome to the chat room! \n"
-              "Waiting for response from client.\n"
-              "Type EXIT to quit")
-        while True:
-            data = conn.recv(1024)
-            data.decode()
-            if not data:
-                break
-            print(data)
-            msg = input("Enter Msg: ")
-            msg = msg.encode()
-            conn.sendall(msg)
+isServerRunning = True
+
+while isServerRunning:
+
+    # Variable used to determine if the chat session is ending
+    isConnected = True
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+
+        # Await a connection
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"connection from {addr}")
+            print("Welcome to the chat room! \n"
+                  "Type EXIT to end session \n"
+                  "Type QUIT to close server \n"
+                  "Put a period at the end of chat to send message\n\n"
+                  "Waiting for response from client.\n"
+                  )
+
+            # While the chat session is still going
+            while isConnected:
+
+                # Receive message from client
+                data = conn.recv(1024)
+                data = data.decode()
+                if not data:
+                    break
+
+                # If the client wants to end the session, then we close the connection
+                # We set isConnected to false, so we can go back to awaiting a connection
+                if data == "EXIT":
+                    isConnected = False
+                    print("\nAwaiting another connection\n")
+                    conn.close()
+
+                else:
+                    print("From Client: " + data)
+                    msg = input("Enter Msg: ")
+
+                    # If the user wants to close the server
+                    if msg == "QUIT":
+
+                        # Send message to client
+                        msg = msg.encode()
+                        conn.sendall(msg)
+
+                        isServerRunning = False
+                        isConnected = False
+
+                        # Close connection
+                        conn.close()
+
+                    # If the server wants to end the session
+                    elif msg == "EXIT":
+
+                        # Send message to client
+                        msg = msg.encode()
+                        conn.sendall(msg)
+
+                        # Close connection
+                        conn.close()
+                        print("\nAwaiting another connection\n")
+                        isConnected = False
+                    else:
+
+                        # Checks to see if the client wants to send their message.
+                        # Continuously asks for input until the client is ready to send
+                        while msg[-1] != ".":
+                            msg += "\n\t\t\t" + input()
+
+                        # Send message to client
+                        msg = msg.encode()
+                        conn.sendall(msg)
+
+
